b8fc1babc0bd8752c2e0d41c7799b9b06bd3762f
quentin deleuil <quentin.deleuil@gmail.com>
2018-12-04 23:42:35 +0100

    new folders for adriano

---
+++ test.py
@@ -0,0 +1,15 @@
+from only_keep_good_files import create_new_parse
+
+create_new_parse(10,10,True,False)
+
+# from face_detection import reshape
+# import os
+# import cv2
+#
+# output=reshape(os.getcwd()+"/all_pict/Aaron_Eckhart/Aaron_Eckhart_0001.jpg",100,False)
+# cv2.imshow('img',output)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+
