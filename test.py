2b9b7ecdc809d6b02fd28cd15c01fb93df6235ce
quentin deleuil <quentin.deleuil@gmail.com>
2018-12-15 23:33:36 +0100

    add new folder for analyse

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ from only_keep_good_files import create_new_parse
 from import_picture import import_processed_pict_from
 import os
 
-treshold = 20
-num_pict = [10, 12, 14, 16, 18, 20]
+threshold_list = [100, 53, 43, 39, 35, 32, 29, 26, 24, 21, 20]
+num_pict = 20
 
-for number in num_pict:
-    create_new_parse(treshold, number, False, False)
+for threshold in threshold_list:
+    create_new_parse(threshold, num_pict, False, False)
 #import_processed_pict_from(os.getcwd() + "/filtered_pict_20", 100, True)
 # from face_detection import reshape
 # import os
